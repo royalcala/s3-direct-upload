4110908f440683cdb711b2cac117d5529f931cc6
Roy Alcalá <alcala.rao@gmail.com>
2022-07-08 12:39:45 -0500

    moved bucket name and region to .env file

--- back/s3.js
+++ back/s3.js
@@ -6,8 +6,8 @@ const randomBytes = promisify(crypto.randomBytes)
 
 dotenv.config()
 
-const region = "us-west-2"
-const bucketName = "rao-s3-direct-upload"
+const region = process.env.AWS_BUCKET_REGION
+const bucketName = process.env.AWS_BUCKET_NAME
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
 
